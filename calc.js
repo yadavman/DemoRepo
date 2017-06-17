f91863a15a4ea3b92c674de91d2fe23b815a4c31
myName <myEmail@bla.com>
2017-06-18 00:14:12 +0530

    added new files

---
+++ calc.js
@@ -0,0 +1,36 @@
+console.log("Unit testing with NodeUnit Module");
+
+module.exports = {
+
+	add: function(val1,val2){
+		checkArguments(val1,val2);
+		return val1 + val2;
+	},
+	subtract: function(val1,val2){
+		checkArguments(val1,val2);
+		return val1 - val2;
+	},
+	multiply: function(val1,val2){
+		checkArguments(val1,val2);
+		return val1*val2;
+	},
+	divide: function(val1,val2){
+		checkArguments(val1,val2);
+		return val1 / val2;
+	}
+	
+};
+
+
+
+
+function checkArguments(val1,val2)
+{
+	if (isNaN(val1)) {
+		throw new Error("Arg1 invalid");
+	}
+	if (isNaN(val2)) {
+		throw new Error("Arg2 Invalid");
+
+	}
+}
